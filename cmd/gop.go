fffa10b1b74859f5ebe7a28b359b38bba446fa57
akadir <akadirkkoc@gmail.com>
2023-07-19 22:38:36 +0300

    chore: version 0.8.1

--- cmd/gop.go
+++ cmd/gop.go
@@ -28,7 +28,7 @@ func Run() {
 
 	app := &cli.App{
 		Name:                 "gop",
-		Version:              "0.8.0",
+		Version:              "0.8.1",
 		Usage:                "gop opens current git repository's remote url on browser.",
 		EnableBashCompletion: true,
 		Authors: []*cli.Author{
